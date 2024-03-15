16ab17aad68a5513d02d097f09dfba75ba357788
sheriffrude <rudeskatrojan@gmail.com>
2024-03-15 10:59:10 -0500

    cambio de grupos y usuarios

---
+++ login/templatetags/custom_tags.py
@@ -0,0 +1,6 @@
+from django import template
+register = template.Library()
+
+@register.filter(name='user_belongs_to_group')
+def user_belongs_to_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
