a1f22745bfb76379904678eadc1ae6b7820dc3c8
sheriffrude <rudeskatrojan@gmail.com>
2025-01-20 14:34:43 -0500

    cambios tasks

--- login/tasks.py
+++ login/tasks.py
@@ -5,17 +5,13 @@ import subprocess
 from celery.schedules import crontab
 from celery import shared_task
 import django
-from .script import main
+
 from .script2 import main2
 from .script3 import main3
 from .script4 import main4
 from django.db import connection
 from django.conf import settings
 
-
-@shared_task
-def ejecutar_script():
-    main()
 
 @shared_task
 def ejecutar_script2():
